807084bfe3624c3c0ddc0454f03e3fb907e5023a
Dale Sinder <sinder@illinois.edu>
2022-05-17 09:24:12 -0500

    Use rleative path rather than absolute path

--- Notes2022/Shared/copygrpc.cmd
+++ Notes2022/Shared/copygrpc.cmd
@@ -1,1 +1,1 @@
-copy E:\Projects\2022gRPC\Notes2022-gRPC\Notes2022\Shared\obj\Debug\net6.0\Protos\*.cs E:\Projects\2022gRPC\Notes2022-gRPC\Notes2022\Proto\Protos\
+copy .\obj\Debug\net6.0\Protos\*.cs ..\Proto\Protos\
