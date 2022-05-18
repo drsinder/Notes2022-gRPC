4ee61af73fc55803f2494122525a3cb21ee2a73b
Dale Sinder <sinder@illinois.edu>
2022-05-18 09:29:32 -0500

    Allow abberviated I/O specs.

--- Notes2022/TextToJson/Program.cs
+++ Notes2022/TextToJson/Program.cs
@@ -9,9 +9,30 @@
         {
             Console.WriteLine("Converting...");
 
+            string outfile = string.Empty;
+            string infile = string.Empty;
+
+            if (args.Count() > 0)
+            {
+                infile = args[0];
+                if (!infile.ToLower().EndsWith(".txt"))
+                {
+                    infile += ".txt";
+                }
+                       
+            }
+            if (args.Count() > 1)
+            {
+                outfile = args[1];
+            }
+            else
+            {
+                outfile = infile.Replace(".txt", ".json");
+            }
+
             Notes2022.TextToJson.Manager.Importer imp = new Notes2022.TextToJson.Manager.Importer();
             
-            _ = imp.Import(args[0], args[1]).GetAwaiter().GetResult();
+            _ = imp.Import(infile, outfile).GetAwaiter().GetResult();
 
         }
     }
