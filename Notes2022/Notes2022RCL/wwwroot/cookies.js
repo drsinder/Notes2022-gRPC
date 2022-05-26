a9b105ff3a02eb54f5c42076bdd4f378de19d12d
Dale Sinder <sinder@illinois.edu>
2022-05-25 19:52:42 -0500

    Copy wwwroot assets to RCL

---
+++ Notes2022/Notes2022RCL/wwwroot/cookies.js
@@ -0,0 +1,46 @@
+﻿// ***********************************************************************
+// Assembly         : Notes2022.Client
+// Author           : Dale Sinder
+// Created          : 04-30-2022
+//
+// Last Modified By : Dale Sinder
+// Last Modified On : 05-01-2022
+// ***********************************************************************
+// <copyright file="cookies.js" company="Notes2022.Client">
+//     Copyright (c) 2022 Dale Sinder. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+export function CreateCookie(name, value, hours) {
+    var expires;
+    if (hours) {
+            /// <var>The date</var>
+            var date = new Date();
+            date.setTime(date.getTime() + (hours * 60 * 60 * 1000));
+            expires = "; Expires=" + date.toUTCString();
+        }
+        else {
+            expires = "";
+        }
+        document.cookie = name + "=" + value + expires + "; path=/";
+    }
+
+export function ReadCookie(cname) {
+    var name = cname + "=";
+    /// <var>The decoded cookie</var>
+    var decodedCookie = decodeURIComponent(document.cookie);
+    /// <var>The ca</var>
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        /// <var>The c</var>
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
---
+++ Notes2022/Notes2022RCL/wwwroot/scripts.js
@@ -0,0 +1,26 @@
+﻿// ***********************************************************************
+// Assembly         : Notes2022.Client
+// Author           : Dale Sinder
+// Created          : 04-20-2022
+//
+// Last Modified By : Dale Sinder
+// Last Modified On : 10-31-2021
+// ***********************************************************************
+// <copyright file="scripts.js" company="Notes2022.Client">
+//     Copyright (c) 2022 Dale Sinder. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+export function saveAsFile(filename, bytesBase64) {
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = "data:application/octet-stream;base64," + bytesBase64;
+    document.body.appendChild(link); // Needed for Firefox
+    link.click();
+    document.body.removeChild(link);
+}
+
+function getWidth(x) {
+    return window.innerWidth;
+}
