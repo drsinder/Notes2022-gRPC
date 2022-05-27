e19a141fa5f548f06134e3d530780b66340232b7
Dale Sinder <sinder@illinois.edu>
2022-05-26 23:14:27 -0500

    LAst hangfire menu attempt...

--- Notes2022/Notes2022RCL/Menus/NavMenu.razor.cs
+++ Notes2022/Notes2022RCL/Menus/NavMenu.razor.cs
@@ -325,8 +325,8 @@ namespace Notes2022RCL.Menus
                 {
                     new() {Id = "NoteFiles", Text = "NoteFiles"},
                     new() {Id = "Roles", Text = "Roles"},
-                    new() {Id = "Hangfire", Text = "Jobs-Dashboard"}
-                    //new() {Id = "Hangfire", Text = "Hangfire"}
+                    new() {Id = "Hangfire", Text = "Jobs-Dashboard"},
+                    new() {Id = "Roles", Text = "Roles"}
                 };
                 
                 //if (Globals.IsMaui)
