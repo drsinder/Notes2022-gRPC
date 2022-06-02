855fa1b61fa7ea5034bcc247770702ce62039234
Dale Sinder <sinder@illinois.edu>
2022-06-02 17:04:10 -0500

    Documentation

--- Notes2022/Notes2022MauiLib/MauiFileActions.cs
+++ Notes2022/Notes2022MauiLib/MauiFileActions.cs
@@ -7,6 +7,13 @@ namespace Notes2022MauiLib
     // All the code in this file is included in all platforms.
     public class MauiFileActions
     {
+        /// <summary>
+        /// Saves to file and clip board.
+        /// </summary>
+        /// <param name="filename">The filename.</param>
+        /// <param name="data">The data.</param>
+        /// <param name="cliptoo">if set to <c>true</c> clip saved too.</param>
+        /// <returns>System.String.</returns>
         public async Task<string> SaveToFileAndClipBoard(string filename, byte[] data, bool cliptoo)
         {
             string textData = Encoding.Default.GetString(data);
@@ -15,18 +22,26 @@ namespace Notes2022MauiLib
                 await Clipboard.Default.SetTextAsync(textData);
 
             string file = Path.Combine(FileSystem.Current.AppDataDirectory, filename);
-
             await File.WriteAllTextAsync(file, textData);
 
             return file;
         }
 
+        /// <summary>
+        /// Deletes the file.
+        /// </summary>
+        /// <param name="filename">The filename.</param>
         public void DeleteFile(string filename)
         {
             string file = Path.Combine(FileSystem.Current.AppDataDirectory, filename);
             File.Delete(file);
         }
 
+        /// <summary>
+        /// Reads from file.
+        /// </summary>
+        /// <param name="filename">The filename.</param>
+        /// <returns>System.String.</returns>
         public async Task<string> ReadFromFile(string filename)
         {
             string file = Path.Combine(FileSystem.Current.AppDataDirectory, filename);
@@ -38,11 +53,10 @@ namespace Notes2022MauiLib
             return null;
         }
 
-        //public async Task SaveFileToClipBoard(string data)
-        //{
-        //    await Clipboard.Default.SetTextAsync(data);
-        //}
-
+        /// <summary>
+        /// Reads the clipboard.
+        /// </summary>
+        /// <returns>System.String.</returns>
         public async Task<string> ReadClipboard()
         {
             if (Clipboard.Default.HasText)
@@ -52,9 +66,6 @@ namespace Notes2022MauiLib
             else
                 return null;
         }
-
-
     }
-
 }
 
