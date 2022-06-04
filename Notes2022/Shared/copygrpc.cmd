e548680b8c9a01117b510330028f8550642a63f9
Dale Sinder <sinder@illinois.edu>
2022-06-04 00:21:46 -0500

    comment out

--- Notes2022/Shared/copygrpc.cmd
+++ Notes2022/Shared/copygrpc.cmd
@@ -1,3 +1,3 @@
-# protoc --doc_out=. --doc_opt=html,grpc.html protos/*.proto
+REM protoc --doc_out=. --doc_opt=html,grpc.html protos/*.proto
 copy .\obj\Debug\net6.0\Protos\*.cs ..\Proto\Protos\
-# copy .\grpc.html ..\..\..\Notes2022Docs\Solution\
+REM copy .\grpc.html ..\..\..\Notes2022Docs\Solution\
