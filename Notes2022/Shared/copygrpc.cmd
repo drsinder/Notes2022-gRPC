8d64f0497df0437bb2641814f734a6e5de92733d
Dale Sinder <sinder@illinois.edu>
2022-06-04 00:38:40 -0500

    restore docs

--- Notes2022/Shared/copygrpc.cmd
+++ Notes2022/Shared/copygrpc.cmd
@@ -1,3 +1,3 @@
-REM protoc --doc_out=. --doc_opt=html,grpc.html protos/*.proto
+protoc --doc_out=. --doc_opt=html,grpc.html protos/*.proto
 copy .\obj\Debug\net6.0\Protos\*.cs ..\Proto\Protos\
-REM copy .\grpc.html ..\..\..\Notes2022Docs\Solution\
+copy .\grpc.html ..\..\..\Notes2022Docs\Solution\
